07e4cd3ece5fe969883bf943f4e59f8b30493287
Hrishabh Sharma <34400111+Sharma-Hrishabh@users.noreply.github.com>
2021-07-07 16:08:45 +0530

    Add Pausable and Burn functionality

--- main.sol
+++ main.sol
@@ -1,29 +1,43 @@
-pragma solidity ^ 0.8.0;
+/*
+    * @dev Assume supply=1 everywhere when passing arguments.
+    * Sample bytes32: 0x6162636400000000000000000000000000000000000000000000000000000000
+*/
+
+pragma solidity ^ 0.8 .0;
 
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
-import "../contracts/security/Pausable.sol";
-
-contract THEWISARTS is Pausable,ERC1155("https://raw.githubusercontent.com/The-Wise-Org/document-meta/main/{id}") {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/Pausable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 
 // 0x6162636400000000000000000000000000000000000000000000000000000000
 
+contract THEWISARTS is Pausable, Ownable, ERC1155("https://raw.githubusercontent.com/The-Wise-Org/document-meta/main/{id}") {
+
     uint private totalRegisteredAssets;
     uint private totalAssetsForSale;
-    
+
     mapping(uint256 => bytes) private metadataHash;
-    
+
     constructor() {
         totalRegisteredAssets = 0;
-        totalAssetsForSale = 0;
+        totalAssetsForSale = 0;   
     }
 
+    /*
+    * @dev Mint Assets to the address of _holder.
+    * Requirements:
+    * Can only be called by the deployer of the smart contract.
+    */
     function mintAsset(uint _supply, address _holder, uint256 _id, bytes memory _data) public {
         require(metadataHash[_id].length == 0);
         _mint(_holder, _id, _supply, _data);
     }
 
+    /*
+    *@dev Called before every token transfer, maps the metadataHash of an asset with the token/asset id  
+    */
     function _beforeTokenTransfer(address _operator, address _from, address _to, uint256[] memory ids, uint256[] memory amount, bytes memory data) override internal {
         // require(msg.sender == _from);
         // require(balanceOf(_from, ids[0]) > 0);
@@ -31,18 +45,50 @@ contract THEWISARTS is Pausable,ERC1155("https://raw.githubusercontent.com/The-W
         require(!paused(), "ERC1155Pausable: token transfer while paused");
     }
 
+    /*
+     * @dev returns the metaDataHash for the token/asset ID
+     */
+
     function getTokenMetadataHash(uint256 _id) view public returns(string memory) {
         return string(metadataHash[_id]);
     }
 
+     /*
+     * @dev returns the baseURI.
+     * Doesn't depends on the token-id and returns same thing.
+     */
     function getBaseURI(uint256 _id) view public returns(string memory) {
-        //will be same for all token ids, as base uri is same
         return super.uri(_id);
     }
 
-    function transfer(address _to, uint256 _id, uint256 _amount, bytes memory _data) public{
+     /*
+     * @dev Tranfers the asset to new address.
+     * Can be called by the asset holder.
+     */
+    function transfer(address _to, uint256 _id, uint256 _amount, bytes memory _data) public {
         super.safeTransferFrom(msg.sender, _to, _id, _amount, _data);
     }
 
-    //TODO: Overriding safetransfer and aftercheck
+    /*
+    * @dev pause the wisarts contract.
+    */
+    function makePause() public onlyOwner {
+        super._pause();
+    }
+
+    /*
+     * @dev un-pause the wisarts contract.
+     */
+    function makeUnpause() public onlyOwner{
+        super._unpause();
+    } 
+
+    /*
+     * @dev Burns the token/asset with the given tokenid.
+     */
+    function burnAsset(address account,uint256 id,uint256 amount) public{
+        super._burn(account,id,amount);
+        metadataHash[id]=bytes("0x0");
+    }
+
 }
