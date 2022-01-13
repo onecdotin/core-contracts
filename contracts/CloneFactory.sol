8ed0fa1a2e30662304aabcf7514f349e39cddb86
Sharma-Hrishabh <30hrs1999@gmail.com>
2022-01-13 23:23:00 +0530

    add transferOwnership

--- contracts/CloneFactory.sol
+++ contracts/CloneFactory.sol
@@ -13,9 +13,11 @@ contract OnecFactory is OwnableUpgradeable {
         tokenImplementation = address(new Onec());
     }
 
-    function createOnec(string memory _baseURI,string memory _contractMetadata) external onlyOwner returns (address) {
+    function createOnec(string memory _baseURI,string memory _contractMetadata,address _newOwner) external onlyOwner returns (address) {
+        require(_newOwner != address(0));
         address clone = ClonesUpgradeable.clone(tokenImplementation);
         Onec(clone).initialize(_baseURI, _contractMetadata);
+        Onec(clone).transferOwnership(_newOwner);
         return clone;
     }
 
