91e1dcad0c73db6da43450822854b28535b5d749
Sharma-Hrishabh <30hrs1999@gmail.com>
2022-04-02 21:56:02 +0530

    remove deployProxy

--- migrations/2_onec1155_factory.js
+++ migrations/2_onec1155_factory.js
@@ -1,9 +1,6 @@
-// const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-
 const Onec1155Factory = artifacts.require('Onec1155Factory');
 
 module.exports = async function (deployer) {
-  // const instance = await deployProxy(Factory1155,{ deployer, initializer: 'initialize'  });
-  // console.log('Deployed', instance.address);
+
   deployer.deploy(Onec1155Factory);
 };
--- migrations/3_onec721_factory.js
+++ migrations/3_onec721_factory.js
@@ -1,9 +1,5 @@
-// const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-
 const Onec721Factory = artifacts.require('Onec721Factory');
 
 module.exports = async function (deployer) {
-  // const instance = await deployProxy(Factory1155,{ deployer, initializer: 'initialize'  });
-  // console.log('Deployed', instance.address);
   deployer.deploy(Onec721Factory);
 };
